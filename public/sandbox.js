3662937b9bf78d832fddcd36a2b2e75e27ec7141
DavidBai <baiminghan@gmail.com>
2021-11-30 15:39:49 +1300

    set up the workflow by adding tsconfig.json and set up the structure of the project

---
+++ public/sandbox.js
@@ -0,0 +1,6 @@
+"use strict";
+let character = "David";
+let age = 30;
+let isBlackBelt = false;
+let test = "test";
+console.log(test);
---
+++ src/sandbox.ts
@@ -0,0 +1,7 @@
+let character = "David";
+let age = 30;
+let isBlackBelt = false;
+
+let test: string = "test";
+
+console.log(test);
