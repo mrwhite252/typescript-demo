34e264f0227f84d287f46b3ff5e714cf129334bb
DavidBai <baiminghan@gmail.com>
2021-11-30 22:36:06 +1300

    introduce tuples to re-format the string and simplify the code

--- src/app.ts
+++ src/app.ts
@@ -21,12 +21,17 @@ const list = new ListTemplate(ul);
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
+  //   implant tuples to format the code:
+
+  let values: [string, string, number];
+  values = [tofrom.value, details.value, amount.valueAsNumber];
+
   let doc: HasFormatter;
 
   if (type.value === "invoice") {
-    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new Invoice(...values);
   } else {
-    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new Payment(...values);
   }
 
   list.render(doc, type.value, "start");
